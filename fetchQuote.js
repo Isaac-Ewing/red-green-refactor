67823dca514e90fc571ce1c585a88eb3cdf480bc
Isaac <isaac.ewing3@gmail.com>
2021-07-20 12:34:56 -0600

    got a quote

---
+++ fetchQuote.js
@@ -0,0 +1,10 @@
+import fetch from 'node-fetch';
+
+const getQuote = async () => {
+  const data = await fetch('https://futuramaapi.herokuapp.com/api/quotes');
+  const body = await data.json();
+  const result = body.map(quote => quote)[0];
+  return result;
+};
+    
+export default getQuote;
