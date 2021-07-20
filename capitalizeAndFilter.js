fab06499fd0e6a2e26c6fc3316c97fbd79fac1a0
Isaac <isaac.ewing3@gmail.com>
2021-07-20 12:22:26 -0600

    did the refactoring, already had some array methods in there

--- capitalizeAndFilter.js
+++ capitalizeAndFilter.js
@@ -1,9 +1,9 @@
 export default function capitalizeAndFilter(array) {
   let newArr = [];
-  for (let string of array) {
+  array.map(string => {
     string = string.toUpperCase();
     newArr.push(string);
-  }
+  });
   newArr = newArr.filter(string => string[0] !== 'F');
   return newArr;
 }
