045853f6a8a498649de6ab8ca13436421fa0a6a1
Isaac <isaac.ewing3@gmail.com>
2021-07-20 11:22:23 -0600

    made the basic function and got the test to pass

---
+++ copyAndPush.js
@@ -0,0 +1,4 @@
+export default function copyAndPush(array, number) {
+  array.push(number);
+  return array;
+}
--- copyAndPush.test.js
+++ copyAndPush.test.js
@@ -1,3 +1,5 @@
+import copyAndPush from './copyAndPush.js';
+
 describe('returns a copy of an array with a new item pushed to the end', () => {
   it('returns [1, 2, 3, 4]', () => {
     const numbers = [1, 2, 3];
