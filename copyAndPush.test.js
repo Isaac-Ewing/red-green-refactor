fd7216fce2da6c714578c74d4c7c95958fe7b9c3
Isaac <isaac.ewing3@gmail.com>
2021-07-20 11:17:43 -0600

    made the test for the copy and push

---
+++ copyAndPush.test.js
@@ -0,0 +1,9 @@
+describe('returns a copy of an array with a new item pushed to the end', () => {
+  it('returns [1, 2, 3, 4]', () => {
+    const numbers = [1, 2, 3];
+    const expected = [1, 2, 3, 4];
+    const actual = copyAndPush(numbers, 4);
+  
+    expect(actual).toEqual(expected);
+  });
+});
